294b77d8908910a85db500733115473bf85cb577
Roger <blldsp@proton.me>
2025-06-10 18:26:10 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://rogerdotsh.github.io',
-  base: '/leila-site',
+  site: 'https://rogerdotsh.github.io/leila-site'
 })
