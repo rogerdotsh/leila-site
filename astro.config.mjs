4a7281f6125cdff39bd59d3096312d8414ff5634
Roger <blldsp@proton.me>
2025-06-25 21:16:38 +0000

    fix astro.config.mjs
    forgot an apostrophe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://rogerdotsh.github.io/leila-site,
+  site: 'https://rogerdotsh.github.io/leila-site',
   base: 'leila-site',
 })
